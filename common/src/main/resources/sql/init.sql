eee8560795760bb907ecd1edf18e595c64a8fad1
J.K.SAGE <sage417@users.noreply.github.com>
2024-01-30 17:53:10 +0800

    Update init.sql

--- common/src/main/resources/sql/init.sql
+++ common/src/main/resources/sql/init.sql
@@ -12,3 +12,16 @@ CREATE TABLE `t_tenant_db_info` (
                                          `module` varchar(50) NOT NULL DEFAULT '' COMMENT '数据源所属子模块',
                                          PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COMMENT='数据源信息';
+
+CREATE TABLE `t_tenant_db_mapping` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `tenant_code` varchar(100) NOT NULL COMMENT '租户标识',
+  `db_code` varchar(100) NOT NULL COMMENT '数据源标识',
+  `type` varchar(50) NOT NULL COMMENT '数据源类型',
+  `module` varchar(50) NOT NULL DEFAULT '' COMMENT '数据源所属子模块',
+  `create_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+  `update_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3),
+  PRIMARY KEY (`id`),
+  KEY `idx_tenant_code` (`tenant_code`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COMMENT='租户和数据源映射表'
+
