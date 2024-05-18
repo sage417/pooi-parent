ec93291350170adb44b2b7aad6960cb5702e8f92
sage417 <sage417@foxmail.com>
2024-05-18 21:11:19 +0800

    [common] update sql

--- common/src/main/resources/sql/data.sql
+++ common/src/main/resources/sql/data.sql
@@ -1,5 +1,5 @@
 insert into t_tenant_db_info (name, db_code, username, password, url, driver_class)
-values ('ds1', 'ds1', 'sa', '', 'jdbc:h2:mem:pooi-workflow-app1;DB_CLOSE_DELAY=-1', 'org.h2.Driver');
+values ('ds1', 'ds1', 'sa', '', 'jdbc:h2:mem:pooi-workflow-app1;INIT=RUNSCRIPT FROM ''classpath:sql/workflow_extend.sql''', 'org.h2.Driver');
 
 insert into t_tenant_info (tenant_code, tenant_name)
 values ('app1', 'app1');
