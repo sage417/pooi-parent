94365598415b3e80373c89df1ac8e7e63c5ee3e8
sage417 <sage417@foxmail.com>
2024-05-18 21:10:45 +0800

    [workflow] add sqlSessionFactoryBeanCustomizer

--- workflow/src/main/java/app/pooi/workflow/configuration/mybatis/MybatisConfiguration.java
+++ workflow/src/main/java/app/pooi/workflow/configuration/mybatis/MybatisConfiguration.java
@@ -1,10 +1,19 @@
 package app.pooi.workflow.configuration.mybatis;
 
+import com.baomidou.mybatisplus.autoconfigure.SqlSessionFactoryBeanCustomizer;
 import org.apache.ibatis.annotations.Mapper;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.CollectionUtils;
+
 
 @MapperScan(basePackages = "app.pooi.workflow.repository", annotationClass = Mapper.class)
 @Configuration
 class MybatisConfiguration {
+
+    @Bean
+    SqlSessionFactoryBeanCustomizer sqlSessionFactoryBeanCustomizer() {
+        return factoryBean -> factoryBean.setTypeAliases(CollectionUtils.class);
+    }
 }
