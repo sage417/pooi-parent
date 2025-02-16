147147f5a94a720eb11ffe25aec93a2db94b753d
sage417 <sage417@foxmail.com>
2025-02-16 22:53:04 +0800

    [workflow] integration with skywalking logging

--- workflow/src/main/java/app/pooi/workflow/controller/MeController.java
+++ workflow/src/main/java/app/pooi/workflow/controller/MeController.java
@@ -1,5 +1,6 @@
 package app.pooi.workflow.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.core.oidc.StandardClaimNames;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
@@ -8,10 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 public class MeController {
     @GetMapping("/me")
     public UserInfoDto getGreeting(JwtAuthenticationToken auth) {
+        log.info("greeting with traceId");
         return new UserInfoDto(
                 auth.getToken().getClaimAsString(StandardClaimNames.PREFERRED_USERNAME),
                 auth.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList());
