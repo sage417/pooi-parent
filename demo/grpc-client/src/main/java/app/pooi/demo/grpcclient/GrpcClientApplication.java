866ef35ddd9aeefe7e8162d2c6c6842fb7417bc9
sage417 <sage417@foxmail.com>
2025-06-27 12:30:09 +0800

    rename demo projects

---
+++ demo/grpc-client/src/main/java/app/pooi/demo/grpcclient/GrpcClientApplication.java
@@ -0,0 +1,19 @@
+package app.pooi.demo.grpcclient;
+
+import app.pooi.modules.workflow.stubs.HelloWorldServiceGrpc;
+import net.devh.boot.grpc.client.inject.GrpcClient;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcClientApplication.class, args);
+    }
+
+    @GrpcClient("pooi-workflow-core")
+    private HelloWorldServiceGrpc.HelloWorldServiceStub stub;
+
+
+}
