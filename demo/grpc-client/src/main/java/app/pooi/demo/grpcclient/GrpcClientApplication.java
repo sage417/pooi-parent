fa672df0deec1712139afb193c22d8e3e5bb571e
sage417 <sage417@foxmail.com>
2025-06-27 16:24:25 +0800

    [demo] update grpc client

--- demo/grpc-client/src/main/java/app/pooi/demo/grpcclient/GrpcClientApplication.java
+++ demo/grpc-client/src/main/java/app/pooi/demo/grpcclient/GrpcClientApplication.java
@@ -1,21 +1,36 @@
 package app.pooi.demo.grpcclient;
 
+import app.pooi.modules.workflow.stubs.HelloWorldRequest;
 import app.pooi.modules.workflow.stubs.HelloWorldServiceGrpc;
 import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@RestController
 public class GrpcClientApplication {
+
+    @GrpcClient("pooi-workflow-core")
+    private HelloWorldServiceGrpc.HelloWorldServiceBlockingStub stub;
+
 
     public static void main(String[] args) {
         SpringApplication.run(GrpcClientApplication.class, args);
     }
 
-    @GrpcClient("pooi-workflow-core")
-    private HelloWorldServiceGrpc.HelloWorldServiceStub stub;
+
+    @GetMapping("/sayHello")
+    public String greeting(@RequestParam String name) {
+        HelloWorldRequest request = HelloWorldRequest.newBuilder()
+                .setName(name)
+                .build();
+        return stub.sayHello(request).getGreeting();
+    }
 
 
 }
