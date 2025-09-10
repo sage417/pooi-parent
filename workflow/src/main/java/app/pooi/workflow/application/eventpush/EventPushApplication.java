37b7ded5c53c339478546993d7c114ecd6426104
sage417 <sage417@foxmail.com>
2025-09-10 11:13:38 +0800

    [workflow] update EventPushApplication.java

--- workflow/src/main/java/app/pooi/workflow/application/eventpush/EventPushApplication.java
+++ workflow/src/main/java/app/pooi/workflow/application/eventpush/EventPushApplication.java
@@ -1,16 +1,21 @@
 package app.pooi.workflow.application.eventpush;
 
 import app.pooi.basic.workflow.event.EventPayload;
+import app.pooi.tenant.multitenancy.ApplicationInfoHolder;
+import app.pooi.workflow.repository.workflow.EventPushProfileDO;
+import app.pooi.workflow.repository.workflow.EventPushProfileRepository;
 import app.pooi.workflow.repository.workflow.EventRecordDO;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.redisson.api.RFuture;
 import org.redisson.api.RLock;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
 
@@ -18,24 +23,61 @@ import java.util.concurrent.Executor;
 @Component
 public class EventPushApplication {
 
-    @Qualifier("event-push")
-    private Executor executor;
-
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
+    @Resource(name = "event-push")
+    private Executor executor;
+
+    @Resource
+    private EventPushProfileRepository eventPushProfileRepository;
+
+    @Resource
+    private ApplicationInfoHolder applicationInfoHolder;
+
+    @Resource
+    private Map<String, PushStrategy> pushStrategies;
+
+    public void asyncExecute(RLock fairLock, RFuture<Boolean> lockAsync, long tId, List<EventRecordDO> eventRecordDOS) {
+        executor.execute(() -> {
+            // ensure locked
+            boolean acquireLock = ensureAsyncLock(fairLock.getName(), lockAsync, tId);
+            try {
+                pushOrderly(eventRecordDOS);
+            } finally {
+                unlock(acquireLock, fairLock, tId);
+            }
+        });
+    }
 
     @SneakyThrows
-    public void asyncExecute(RLock fairLock, RFuture<Boolean> lockAsync, long tId, List<EventRecordDO> eventRecordDOS) {
-        // ensure locked
-        boolean acquireLock = ensureAsyncLock(fairLock.getName(), lockAsync, tId);
+    private void pushOrderly(List<EventRecordDO> eventRecordDOS) {
+        EventPushProfileDO profileDO = eventPushProfileRepository.getOne(
+                Wrappers.lambdaQuery(EventPushProfileDO.class)
+                        .eq(EventPushProfileDO::getTenantId, applicationInfoHolder.getApplicationCode())
+        );
+
+        if (profileDO == null) {
+            return;
+        }
+
+        PushStrategy pushStrategy = getPushStrategy(profileDO.getType());
+
         for (EventRecordDO eventRecordDO : eventRecordDOS) {
             EventPayload eventPayload = objectMapper.readValue(eventRecordDO.getEvent(), EventPayload.class);
             String bodyString = objectMapper.writeValueAsString(eventPayload);
             log.info("event payload: {}", bodyString);
+
+            pushStrategy.push(eventRecordDO);
         }
-        unlock(acquireLock, fairLock, tId);
     }
 
+    private PushStrategy getPushStrategy(String strategyName) {
+        PushStrategy pushStrategy = pushStrategies.get(strategyName + "PushStrategy");
+        if (pushStrategy == null) {
+            throw new IllegalArgumentException("No push strategy registered for " + strategyName);
+        }
+        return pushStrategy;
+    }
 
     private static void unlock(boolean acquireLock, RLock fairLock, long tId) {
         if (fairLock == null || !acquireLock) {
