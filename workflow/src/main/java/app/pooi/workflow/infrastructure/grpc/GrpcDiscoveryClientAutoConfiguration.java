cb91ee374369caa451edfd1916f758877ad58794
sage417 <sage417@foxmail.com>
2025-09-15 08:42:22 +0800

    [workflow-core] init DiscoveryClientResolverFactory on start up

---
+++ workflow/src/main/java/app/pooi/workflow/infrastructure/grpc/GrpcDiscoveryClientAutoConfiguration.java
@@ -0,0 +1,35 @@
+package app.pooi.workflow.infrastructure.grpc;
+
+import io.grpc.NameResolverProvider;
+import io.grpc.NameResolverRegistry;
+import net.devh.boot.grpc.client.nameresolver.DiscoveryClientResolverFactory;
+import net.devh.boot.grpc.client.nameresolver.NameResolverRegistration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+/**
+ * init NameResolverRegistration because original autoconfiguration init it lazy
+ *
+ * @see net.devh.boot.grpc.client.autoconfigure.GrpcClientAutoConfiguration
+ */
+@Configuration(proxyBeanMethods = false)
+class GrpcDiscoveryClientAutoConfiguration {
+
+    @Bean
+    DiscoveryClientResolverFactory grpcDiscoveryClientResolverFactory(final DiscoveryClient client) {
+        return new DiscoveryClientResolverFactory(client);
+    }
+
+    @Bean
+    NameResolverRegistration grpcNameResolverRegistration(
+            @Autowired(required = false) final List<NameResolverProvider> nameResolverProviders) {
+        final NameResolverRegistration nameResolverRegistration = new NameResolverRegistration(nameResolverProviders);
+        nameResolverRegistration.register(NameResolverRegistry.getDefaultRegistry());
+        return nameResolverRegistration;
+    }
+
+}
