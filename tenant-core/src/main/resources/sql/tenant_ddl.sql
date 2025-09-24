a24b24b9a633cb04e428035d7c0db68815517952
sage417 <sage417@foxmail.com>
2025-09-24 23:46:23 +0800

    [tenant-core] update tenant_ddl.sql

--- tenant-core/src/main/resources/sql/tenant_ddl.sql
+++ tenant-core/src/main/resources/sql/tenant_ddl.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `t_tenant_db_info`
 (
-    `id`           bigint(20) NOT NULL AUTO_INCREMENT,
+    `id`          bigint(20)  NOT NULL AUTO_INCREMENT,
     `name`         varchar(100) NOT NULL DEFAULT '' COMMENT '数据源名',
     `db_code`      varchar(100) NOT NULL DEFAULT '' COMMENT '数据库标识',
     `username`     varchar(100) NOT NULL DEFAULT '' COMMENT '数据源账号',
@@ -9,8 +9,8 @@ CREATE TABLE `t_tenant_db_info`
     `driver_class` varchar(100) NOT NULL DEFAULT '' COMMENT '驱动类',
     `type`         varchar(50)  NOT NULL DEFAULT '' COMMENT '数据源类型',
     `schema_sql`   varchar(200) NOT NULL DEFAULT '' COMMENT 'schema文件',
-    `create_time`  datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP (3),
-    `update_time`  datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP (3) ON UPDATE CURRENT_TIMESTAMP (3),
+    `create_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `update_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3),
     `module`       varchar(50)  NOT NULL DEFAULT '' COMMENT '数据源所属子模块',
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB
@@ -19,24 +19,24 @@ CREATE TABLE `t_tenant_db_info`
 
 CREATE TABLE `t_tenant_info`
 (
-    `id`          bigint(20) NOT NULL AUTO_INCREMENT,
+    `id`          bigint(20)  NOT NULL AUTO_INCREMENT,
     `tenant_code` varchar(100) NOT NULL DEFAULT '' COMMENT '租户标识',
     `tenant_name` varchar(100) not null DEFAULT '' comment '租户名称',
-    `create_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP (3),
-    `update_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP (3) ON UPDATE CURRENT_TIMESTAMP (3)
+    `create_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `update_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3)
 );
 
 CREATE TABLE `t_tenant_db_mapping`
 (
-    `id`          bigint(20) NOT NULL AUTO_INCREMENT,
+    `id`          bigint(20)  NOT NULL AUTO_INCREMENT,
     `tenant_code` varchar(100) NOT NULL DEFAULT '' COMMENT '租户标识',
     `db_code`     varchar(100) NOT NULL DEFAULT '' COMMENT '数据源标识',
     `type`        varchar(50)  NOT NULL DEFAULT '' COMMENT '数据源类型',
     `module`      varchar(50)  NOT NULL DEFAULT '' COMMENT '数据源所属子模块',
-    `create_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP (3),
-    `update_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP (3) ON UPDATE CURRENT_TIMESTAMP (3),
+    `create_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `update_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3),
     PRIMARY KEY (`id`),
-    KEY           `idx_tenant_code` (`tenant_code`) USING BTREE
+    KEY `idx_tenant_code` (`tenant_code`) USING BTREE
 ) ENGINE = InnoDB
   AUTO_INCREMENT = 0
   DEFAULT CHARSET = utf8mb4 COMMENT ='租户和数据源映射表'
